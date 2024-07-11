31261a0e2c8b70cb69122951b096b92c1e90b1ba
Зайцев Иван <izaitsev@innoseti.ru>
2024-07-11 21:09:15 +0200

    HW06 done

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,38 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	// Проверка на отсутствие этапов
+	if len(stages) == 0 {
+		chData := make(Bi)
+		defer close(chData)
+		return chData
+	}
+
+	chOut := in
+
+	for _, stage := range stages {
+		chData := make(Bi)
+
+		go func(chData Bi, chOut Out) {
+			defer close(chData)
+
+			for {
+				select {
+				// Реализация остановки пайплайна
+				case <-done:
+					return
+				case v, ok := <-chOut:
+					// Канал закрыт - заканчиваем
+					if !ok {
+						return
+					}
+					chData <- v
+				}
+			}
+		}(chData, chOut)
+
+		chOut = stage(chData)
+	}
+
+	return chOut
 }
